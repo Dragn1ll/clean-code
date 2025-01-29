2cb474a18ab8e856fa3a21195f85be35c8ffe28d
Dragn1ll <abdulloev-2005@mail.ru>
2025-01-29 16:17:19 +0300

    did docker

--- WebServer/Application/Dto/DocumentListDto.cs
+++ WebServer/Application/Dto/DocumentListDto.cs
@@ -5,6 +5,6 @@ namespace Application.Dto;
 public class DocumentListDto
 {
     [Required] public Guid DocumentId { get; set; }
-    [Required] public string Title { get; set; }
+    [Required] public string? Title { get; set; }
     [Required] public DateTime Created { get; set; }
 }
--- WebServer/Application/Dto/DocumentRedactorDto.cs
+++ WebServer/Application/Dto/DocumentRedactorDto.cs
@@ -2,7 +2,7 @@ namespace Application.Dto;
 
 public class DocumentRedactorDto
 {
-    public string Title { get; set; }
-    public string Text { get; set; }
-    public string ConvertedText { get; set; }
+    public string? Title { get; set; }
+    public string? Text { get; set; }
+    public string? ConvertedText { get; set; }
 }
